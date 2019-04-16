fb8ba6106307bc6f9cb6b35ab9873fa398326fd9
andy <arteysoft@gmail.com>
2019-04-16 16:11:46 +0000

    clase03

---
+++ src/main/java/educacionit/entities/Empleado.java
@@ -0,0 +1,39 @@
+package educacionit.entities;
+
+import javax.persistence.*;
+
+@Entity
+// @DiscriminatorValue("EM")
+@Table(name="empleados")
+public class Empleado extends Persona {
+    @Column(name="salario")
+    private Float salario;
+
+    public Empleado() {
+    }
+
+    public Empleado(String id, String nombre, Float salario) {
+        super(id, nombre);
+        this.salario = salario;
+    }
+
+    public Float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Float salario) {
+        this.salario = salario;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado{" + "id=" + id + ", nombre=" + nombre + '}' +
+               "Empleado{" + "salario=" + salario + '}';
+    }
+
+    
+    
+    
+    
+    
+}
